5bab7c71380e5d46d0dac5d2e98f0842ba094129
vedantpatel1997 <vedantp9@gmail.com>
2023-11-26 15:13:38 -0500

    update prod environment api url

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiAddress: 'https://localhost:7026/api/',
+  // apiAddress: 'https://localhost:7026/api/',
 
-  // apiAddress: 'https://libraryprojectapi.azurewebsites.net/api/',
+  apiAddress: 'https://libraryprojectapi.azurewebsites.net/api/',
 };
